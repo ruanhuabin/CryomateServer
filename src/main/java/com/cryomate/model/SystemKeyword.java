c533a9c195df2c103442787f17ab264f8474f9b1
huabin ruan <ruanhuabin@tsinghua.edu.cn>
2019-11-13 11:17:36 +0800

    Add CryomateServer to repo

---
+++ src/main/java/com/cryomate/model/SystemKeyword.java
@@ -0,0 +1,89 @@
+package com.cryomate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "SYS_Keyword")
+public class SystemKeyword {
+	
+	private String ID;
+	private String keyword;
+	private String dataType;
+	private String defaultValue;
+	private String description;
+	
+	
+	
+	@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	
+	@Column(name = "keyword", nullable = false)
+	public String getKeyword() {
+		return keyword;
+	}
+	
+	
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	
+	@Column(name="datatype", nullable=false)
+	public String getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	
+	@Column(name = "defaultvalue", nullable = false)
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+	
+	@Column(name = "description", nullable = false)
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+//	public SystemKeyword()
+//	{
+//		
+//	}
+//	
+//	public SystemKeyword(String iD, String keyword, String dataType, String defaultValue, String description) {
+//		
+//		ID = iD;
+//		this.keyword = keyword;
+//		this.dataType = dataType;
+//		this.defaultValue = defaultValue;
+//		this.description = description;
+//	}
+//	@Override
+//	public String toString() {
+//		return "SystemKeywords [ID=" + ID + ", keyword=" + keyword + ", dataType=" + dataType + ", defaultValue="
+//				+ defaultValue + ", description=" + description + "]";
+//	}
+//	
+	
+
+}
---
+++ src/main/java/com/cryomate/repository/SystemKeywordRepository.java
@@ -0,0 +1,12 @@
+package com.cryomate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cryomate.model.SystemKeyword;
+
+
+@Repository
+public interface SystemKeywordRepository extends JpaRepository<SystemKeyword, String> {
+
+}
