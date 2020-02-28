28f00c3975cdc6813a2ee3f8d8a9e7340c98fa63
ruanhuabin <ruanhuabin@tsinghua.edu.cn>
2020-02-28 12:54:55 +0800

    add script context path to register.sh when scp to loginnode

--- warehouse/script/register.sh
+++ warehouse/script/register.sh
@@ -19,21 +19,21 @@ loginNode=10.10.103.111
 
 str="shuangqing604@2020"
 #Copy create user script to $loginNode
-sshpass -p${str} scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./create_user.sh root@${loginNode}:/tmp/create_user.sh
+sshpass -p${str} scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./warehouse/script/create_user.sh root@${loginNode}:/tmp/create_user.sh
 
 sleep 1
 
 #Running user creating related operation by user root
-sshpass -p $str ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${loginNode} "bash /tmp/create_user.sh ${groupName} ${userName} ${password} ${homeDirPrefix} ${workDir} ${dataDir}"
+sshpass -p $str ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${loginNode} "chmod u+x /tmp/create_user.sh;bash /tmp/create_user.sh ${groupName} ${userName} ${password} ${homeDirPrefix} ${workDir} ${dataDir}"
 
 #Copy ssh password-less script to $loginNode 
-sshpass -p${str} scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./ssh_passless.sh root@${loginNode}:/tmp/ssh_passless.sh
+sshpass -p${str} scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./warehouse/script/ssh_passless.sh root@${loginNode}:/tmp/ssh_passless.sh
 
 sleep 1
 
 # Running ssh password-less operation on $loginNode
 userPassword=${password}
-sshpass -p ${userPassword} ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${userName}@${loginNode} "bash /tmp/ssh_passless.sh"
+sshpass -p ${userPassword} ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${userName}@${loginNode} "chmod u+x /tmp/ssh_passless.sh;bash /tmp/ssh_passless.sh"
 
 # Update NIS info
 sshpass -p $str ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${loginNode} "make -C /var/yp/ "
